d51b4c132cbe1836931a71a005c72e06a514cbb9
Artyom Sadyrin <sadyryn@icloud.com>
2018-02-08 20:09:57 +0300

    Reloaded data in TableView while scrolling, clean up code

--- find-fibonachi/find-fibonachi/ViewController.swift
+++ find-fibonachi/find-fibonachi/ViewController.swift
@@ -15,20 +15,21 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     @IBOutlet weak var fibonachiTable: UITableView!
     @IBOutlet weak var fiboSearchBar: UISearchBar!
     
-    var filteredFibo = [String]()
+    var filteredFibonachi = [String]()
     var numbers = [BigInt]()
+    var N = 20
     
     override func viewDidLoad() {
         super.viewDidLoad()
         fibonachiTable.dataSource = self
         fibonachiTable.delegate = self
         fiboSearchBar.delegate = self
-        numbers = createFiboArray(100)
-        filteredFibo = numbers.map( { String($0) } )
+        numbers = createFibonachiArray(N)
+        filteredFibonachi = numbers.map( { String($0) } )
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return filteredFibo.count
+        return filteredFibonachi.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -36,26 +37,37 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? FibonachiTableViewCell else {
             fatalError("The dequeued cell is not an instance of ListTableViewCell.")
         }
-        let number = filteredFibo[indexPath.row]
+        let number = filteredFibonachi[indexPath.row]
         cell.resultLabel.text = number
         return cell
     }
     
-    func returnFibonachiTableToNormal() {
-        filteredFibo = numbers.map( { String($0) } )
+    func refreshFibonachiTable() {
+        filteredFibonachi = numbers.map( { String($0) } )
         fibonachiTable.reloadData()
     }
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         guard !searchText.isEmpty else {
-            returnFibonachiTableToNormal()
+            refreshFibonachiTable()
             return
         }
         let positionOfNumber = Int(searchText)
         if let positionOfNumber = positionOfNumber {
-            filteredFibo = [String(getFibonachi(positionOfNumber))]
+            filteredFibonachi = [String(getFibonachiNumber(positionOfNumber))]
         }
         fibonachiTable.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        let lastItemPosition = numbers.count - 1
+        if indexPath.row == lastItemPosition {
+            for index in (lastItemPosition + 1)...(lastItemPosition + 21) {
+                let newFibonachiNumber = getFibonachiNumber(index)
+                numbers.append(newFibonachiNumber)
+            }
+            refreshFibonachiTable()
+        }
     }
     
     func searchBar(_ searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
@@ -66,10 +78,10 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         searchBar.text = nil
         searchBar.endEditing(true)
         searchBar.resignFirstResponder()
-        returnFibonachiTableToNormal()
+        refreshFibonachiTable()
     }
     
-    func getFibonachi(_ n: Int) -> BigInt {
+    func getFibonachiNumber(_ n: Int) -> BigInt {
         var num1: BigInt = 0
         var num2: BigInt = 1
         for _ in 1..<n {
@@ -79,12 +91,13 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         return num1
     }
     
-    func createFiboArray(_ index: Int) -> [BigInt] {
+    func createFibonachiArray(_ index: Int) -> [BigInt] {
         var temp = [BigInt]()
         for i in 0..<index {
-            temp.append(getFibonachi(i + 1))
+            temp.append(getFibonachiNumber(i + 1))
         }
         return temp
     }
+    
     
 }
